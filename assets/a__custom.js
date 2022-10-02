c1df6382c513cd0be673d46061417630f1e70bf7
machitake <glory.tm5@gmail.com>
2022-10-02 18:27:52 +0900

    #insta-feed
    インスタフィードにクラス名を追加するjavascript

--- assets/a__custom.js
+++ assets/a__custom.js
@@ -72,7 +72,17 @@ function fadeAnime(){
     // 画面内に入ったらfadeInというクラス名を追記
     }
   });
-
+  
+  $('#insta-feed').each(function(){ //fadeInUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');
+    // 画面内に入ったらfadeInというクラス名を追記
+    }
+  });
+  
 }
 
 // 画面をスクロールをしたら動かしたい場合
